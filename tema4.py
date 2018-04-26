f98945b9ae566d685d98afcafb603236c19a69a2
Moscu George <geodasilva123@gmail.com>
2018-04-26 12:18:31 +0300

    eh

--- tema4.py
+++ tema4.py
@@ -17,7 +17,7 @@ content_file_array.pop(0)
 B = [float(content_file_array[i]) for i in range(0, length)]
 
 elements = [[] for i in range(0, length)]
-index =  [[] for i in range(0, length)]
+index = [[] for i in range(0, length)]
 diagonal = []
 
 for i in range(length, len(content_file_array)):
@@ -32,6 +32,9 @@ for i in range(length, len(content_file_array)):
     elements[tmp[1]] += [tmp[0]]
     index[tmp[1]] += [tmp[2]]
 
+elements = [[12, 3, -5], [1, 5, 3], [3, 7, 13]]
+index = [[0, 1, 2], [0, 1, 2], [0, 1, 2]]
+B = [1, 28, 76]
 
 diagonal_has_0_elements = False
 for i in range(0, len(diagonal)):
@@ -45,10 +48,17 @@ def equal(previous, nxt):
         p = previous[i]
         n = nxt[i]
 
+        if abs(p) - abs(int(p)) < precision or 1 - (abs(p) - abs(int(p))) < precision:
+            p = int(p)
+
+        if abs(n) - abs(int(n)) < precision or 1 - (abs(n) - abs(int(n))) < precision:
+            n = int(n)
+        # print(p, n)
         if p != n:
-            print(p, n)
+            # print(previous, nxt)
             return False
     return True
+
 
 def getIndex(i):
     for j in range(0, len(index[i])):
@@ -56,29 +66,29 @@ def getIndex(i):
             return j
     return 0
 
+
 if not diagonal_has_0_elements:
-    X = [0 for i in B]
-    X_prev = [i+1 for i in X]
+    X = [0, 0, 0]  # [0 for i in B]
+    X_prev = [i + 1 for i in X]
     counter = 0
 
     while not equal(X_prev, X):
+        X_prev = copy.copy(X)
         for i in range(0, len(elements)):
-            tmp = 0.0
+            tmp = B[i]
+
             for j in range(0, len(index[i])):
-                if i == index[j]:
+                if i == index[i][j]:
                     continue
 
-                tmp -= elements[i][j] * X[i]
-            
-            tmp += B[i]
+                tmp -= elements[i][j] * X[index[i][j]]
+                # print("elements", elements[i][j], X[index[i][j]])
+
             tmp /= elements[i][getIndex(i)]
+            # print("tmp", tmp, B[i])
+            # print(X[i], B[i], elements[i][getIndex(i)], tmp)
             X[i] = tmp
 
-        X_prev = copy.copy(X)
         counter += 1
         print(counter)
-        
-
-
-
-    
+    print(X)
