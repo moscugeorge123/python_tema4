856130a59c29d9995e1da46a6213e11893defe64
Moscu George <geodasilva123@gmail.com>
2018-04-26 13:22:59 +0300

    asd

--- tema4.py
+++ tema4.py
@@ -1,94 +1,100 @@
 import copy
 
-f = open('m_rar_2018_5.txt', 'r')
-content_file = f.read()
-f.close()
+for fisier in [1, 2, 3, 4]:
+    f = open('m_rar_2018_' + str(fisier) + '.txt', 'r')
+    content_file = f.read()
+    f.close()
 
-precision = 1
-for i in range(0, 10):
-    precision /= 10
+    precision = 1
+    for i in range(0, 10):
+        precision /= 10
 
-content_file_array = content_file.split('\n')
-content_file_array = [a for a in content_file_array if a != '']
+    content_file_array = content_file.split('\n')
+    content_file_array = [a for a in content_file_array if a != '']
 
-length = int(content_file_array[0])
-content_file_array.pop(0)
+    length = int(content_file_array[0])
+    content_file_array.pop(0)
 
-B = [float(content_file_array[i]) for i in range(0, length)]
+    B = [float(content_file_array[i]) for i in range(0, length)]
 
-elements = [[] for i in range(0, length)]
-index = [[] for i in range(0, length)]
-diagonal = []
+    elements = [[] for i in range(0, length)]
+    index = [[] for i in range(0, length)]
+    diagonal = []
 
-for i in range(length, len(content_file_array)):
-    tmp = content_file_array[i].split(', ')
-    tmp = [float(i) for i in tmp]
-    tmp[1] = int(tmp[1])
-    tmp[2] = int(tmp[2])
+    for i in range(length, len(content_file_array)):
+        tmp = content_file_array[i].split(', ')
+        tmp = [float(i) for i in tmp]
+        tmp[1] = int(tmp[1])
+        tmp[2] = int(tmp[2])
 
-    if (tmp[1] == tmp[2]):
-        diagonal += [tmp[0]]
+        if (tmp[1] == tmp[2]):
+            diagonal += [tmp[0]]
 
-    elements[tmp[1]] += [tmp[0]]
-    index[tmp[1]] += [tmp[2]]
+        elements[tmp[1]] += [tmp[0]]
+        index[tmp[1]] += [tmp[2]]
 
-elements = [[12, 3, -5], [1, 5, 3], [3, 7, 13]]
-index = [[0, 1, 2], [0, 1, 2], [0, 1, 2]]
-B = [1, 28, 76]
+    # elements = [[12, 3, -5], [1, 5, 3], [3, 7, 13], [4, -5, 33]]
+    # index = [[0, 2, 3], [0, 1, 2], [0, 2, 3], [1, 2, 3]]
+    # B = [1, 28, 76, 44]
 
-diagonal_has_0_elements = False
-for i in range(0, len(diagonal)):
-    if abs(diagonal[i]) < precision:
-        diagonal_has_0_elements = True
-        break
+    diagonal_has_0_elements = False
+    for i in range(0, len(diagonal)):
+        if abs(diagonal[i]) < precision:
+            diagonal_has_0_elements = True
+            break
 
+    def equal(previous, nxt):
+        for i in range(0, len(previous)):
+            p = previous[i]
+            n = nxt[i]
 
-def equal(previous, nxt):
-    for i in range(0, len(previous)):
-        p = previous[i]
-        n = nxt[i]
+            # print(p, n)
 
-        if abs(p) - abs(int(p)) < precision or 1 - (abs(p) - abs(int(p))) < precision:
-            p = int(p)
+            # if abs(p - n) > precision:
+            if p != n:
+                # print(previous, nxt)
+                return False
+        return True
 
-        if abs(n) - abs(int(n)) < precision or 1 - (abs(n) - abs(int(n))) < precision:
-            n = int(n)
-        # print(p, n)
-        if p != n:
-            # print(previous, nxt)
-            return False
-    return True
+    def getIndex(i):
+        for j in range(0, len(index[i])):
+            if index[i][j] == i:
+                return j
+        return 0
 
+    if not diagonal_has_0_elements:
+        X = [0 for i in B]
+        X_prev = [i + 1 for i in X]
+        counter = 0
 
-def getIndex(i):
-    for j in range(0, len(index[i])):
-        if index[i][j] == i:
-            return j
-    return 0
+        while not equal(X_prev, X) and counter < 250000:
+            X_prev = copy.copy(X)
+            for i in range(0, len(elements)):
+                tmp = B[i]
 
+                for j in range(0, len(index[i])):
+                    if i == index[i][j]:
+                        continue
 
-if not diagonal_has_0_elements:
-    X = [0, 0, 0]  # [0 for i in B]
-    X_prev = [i + 1 for i in X]
-    counter = 0
+                    tmp -= elements[i][j] * X[index[i][j]]
+                    # print("elements", elements[i][j], X[index[i][j]])
 
-    while not equal(X_prev, X):
-        X_prev = copy.copy(X)
+                tmp /= elements[i][getIndex(i)]
+                # print("tmp", tmp, B[i])
+                # print(X[i], B[i], elements[i][getIndex(i)], tmp)
+                X[i] = tmp
+
+            counter += 1
+
+        print("FISIER ", fisier)
+        print("number of iterations: ", counter)
+        finalResult = 0
         for i in range(0, len(elements)):
-            tmp = B[i]
-
+            s = 0
             for j in range(0, len(index[i])):
-                if i == index[i][j]:
-                    continue
+                s += elements[i][j] * X[index[i][j]]
+            s -= B[i]
+            finalResult += s * s
 
-                tmp -= elements[i][j] * X[index[i][j]]
-                # print("elements", elements[i][j], X[index[i][j]])
-
-            tmp /= elements[i][getIndex(i)]
-            # print("tmp", tmp, B[i])
-            # print(X[i], B[i], elements[i][getIndex(i)], tmp)
-            X[i] = tmp
-
-        counter += 1
-        print(counter)
-    print(X)
+        print("| Ax -  b | ", finalResult)
+        print(' ')
